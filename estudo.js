2fde7ce5ada367ae4e48c86caa70a644e19ed945
gabicardosoo7 <110104768+gabicardosoo7@users.noreply.github.com>
2022-10-06 12:46:21 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -32,3 +32,16 @@ function escreva(){
             document.write("O cubo de " + i + " é "  + i + " x " + i + " = " + (i*i*i) + "<br>")
         }
     }
+
+    function mat(){
+       let v = document.getElementById("valor").value;
+       let j = document.getElementById("juros").value;
+       let t = document.getElementById("meses").value;
+       let r = 0
+       for(let i=1; i<=t; i++){
+          r = v * (1+(j/100));
+          v = r;
+       }
+       document.write("Resultado " + r);
+
+    }
